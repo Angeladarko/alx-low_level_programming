fb1e42f0fdf4f1acb6fc870f5bac52cf08e7d091
Angeladarko <darkoakuffo177@gmail.comgit config --global user.email darkoakuffo177@gmail.com>
2023-02-18 09:23:35 -0800

    alp2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print negative or positive
+ *
+ * Return: print negative or positive
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putcha(alp);
+
+	putchar('\n');
+
+	return (0);
+}
