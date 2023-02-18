8ea72d033bd1a5b75e1e1c37e3bbc488f2da5f2e
Angeladarko <darkoakuffo177@gmail.comgit config --global user.email darkoakuffo177@gmail.com>
2023-02-18 10:31:12 -0800

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print negative or positive
+ *
+ * Return: print negative or positive
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+		if (alp == 'c' || alp == 'y' || alp == 'o')
+			continue;
+		putchar(alp);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
