f849ccb76a9f4e650f990867844da9275533fdfa
Angeladarko <darkoakuffo177@gmail.comgit config --global user.email darkoakuffo177@gmail.com>
2023-03-11 08:20:17 -0800

    task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts();
- * @s:inputs
- * Return: Always 0 (Success) 
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: The string to be printed
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
-	{	
+	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
@@ -15,5 +15,4 @@ void _puts_recursion(char *s)
 	else
 
 		_putchar('\n');
-		return (0);
 }
